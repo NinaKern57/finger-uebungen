65f060589d00b8eda55993540dbda003b8bb9ca5
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-01-16 13:43:07 +0100

    add comments to fueb3-1

--- fueb3-1/fueb3-1.js
+++ fueb3-1/fueb3-1.js
@@ -1,15 +1,17 @@
 import {terminal} from "./dfhi.js";
-let rivers = {};
+let rivers = {}; // rivers is a dictionnary that will persist accross multiple executions of the program
+
 window.main = function(...argv) {
-	let river = argv[0];
-	let length = Number.parseFloat(argv[1]);
-	rivers[river] = length;
-	let html = "<dl>";
-	for(let [r, v] of Object.entries(rivers)) {
+	let river = argv[0]; // name of the river is the first argument
+	let length = Number.parseFloat(argv[1]); // length of the river is the second argument
+	rivers[river] = length; // new river with the name given as key and the length as value
+	// Display the array
+	let html = "<dl>"; // opening a "definition / description" field
+	for(let [r, v] of Object.entries(rivers)) { // go through all the rivers that were added
 		let l = rivers[r];
 		html += `<dt style="font-weight: bold;font-family: Serif">${r}<dt>
-		<dd>${l}</dd>`;
+		<dd>${l}</dd>`; // the river name is the term (dt) and the length is the definition (dd)
 	}
-	html += `</dl><hr/>`;
-	terminal.printh(html);
+	html += `</dl><hr/>`; // close all the tags and add a separator
+	terminal.printh(html); // print the resulting html
 };
